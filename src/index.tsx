dbd98dd1d69aa9d0ab7bfd713596f16a80b15cd6
Eric Clemmons <eric@smarterspam.com>
2020-04-24 16:57:42 -0700

    Use Preact

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,13 @@
+import { h, render } from "preact";
+
 import "./index.css";
+
+const root = document.createElement("nav");
+
+// document.head.appendChild(tailwind);
+document.querySelector("main").prepend(root);
+
+render(
+  <h1 class="bg-gray-dark mb-3 p-2 px-4-fluid text-white">Howdy there</h1>,
+  root
+);
