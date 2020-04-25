f05baaed339fca89eebe8c2236a18c6d29b4c2a7
Eric Clemmons <eric@smarterspam.com>
2020-04-25 11:56:51 -0700

    Add number count to tabbed nav

--- src/Workflow/Button.tsx
+++ src/Workflow/Button.tsx
@@ -1,4 +1,6 @@
 import { h } from "preact";
+import { useEffect, useState } from "preact/hooks";
+import { useUserLogin } from "../hooks/useUserLogin";
 
 type Props = {
   href: "issues" | "pulls";
@@ -7,13 +9,28 @@ type Props = {
 };
 
 export function Button({ children, href, query = null, tooltip = null }) {
-  const url = new URL(href, document.baseURI);
+  const userLogin = useUserLogin();
+  const [count, setCount] = useState(null);
+  const buttonUrl = new URL(href, document.baseURI);
 
   if (query) {
-    url.searchParams.set("q", query);
+    buttonUrl.searchParams.set("q", query);
   }
 
-  const isActive = url.href === window.location.href;
+  const isActive = buttonUrl.href === window.location.href;
+
+  useEffect(() => {
+    const apiUrl = new URL("https://api.github.com/search/issues");
+
+    apiUrl.searchParams.set(
+      "q",
+      query.split("@me").join(userLogin).concat(" repo:aws-amplify/amplify-js")
+    );
+
+    fetch(apiUrl.href)
+      .then((res) => res.json())
+      .then((data) => setCount(data.total_count));
+  }, [buttonUrl.href]);
 
   return (
     <a
@@ -25,10 +42,13 @@ export function Button({ children, href, query = null, tooltip = null }) {
       ]
         .filter(Boolean)
         .join(" ")}
-      href={url.href}
+      href={buttonUrl.href}
       type="button"
     >
-      {children}
+      {children}{" "}
+      {Number.isInteger(count) && (
+        <span class="Counter Counter--gray-light">{count}</span>
+      )}
     </a>
   );
 }
