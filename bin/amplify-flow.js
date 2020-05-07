116ed573370c97c82b5db93e2f3a0111b523f727
Eric Clemmons <eric@smarterspam.com>
2020-05-07 00:28:17 -0700

    Log & attempt to fix npx amplify-flow

--- bin/amplify-flow.js
+++ bin/amplify-flow.js
@@ -5,12 +5,13 @@ const path = require("path");
 
 (async () => {
   try {
-    await open(
-      path.resolve(
-        __dirname,
-        "../dist/Amplify Flow-darwin-x64/Amplify Flow.app"
-      )
+    const appPath = path.resolve(
+      path.dirname(__filename),
+      "../dist/Amplify Flow-darwin-x64/Amplify Flow.app"
     );
+
+    console.info("Opening", appPath, "...");
+    await open(appPath);
   } catch (e) {
     console.error(e);
     process.exit(1);
